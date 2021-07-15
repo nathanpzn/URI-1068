b883a6d6737c61f76ebb4816e2786998f330c23d
Nathanael Martins <61318960+nathanpzn@users.noreply.github.com>
2021-07-15 16:49:05 -0400

    Add files via upload

---
+++ 1068.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#define MAX 1001]
+
+/* Balanço de Parênteses - Pilhas - URI 1068 */
+/* Nathanael Martins R. da Silva */
+
+typedef struct cel {
+	char chave;
+	struct cel *prox;
+} celula;
+
+void empilha_enc_C(char y, celula *t)
+{
+	celula *nova;
+	nova = (celula *) malloc(sizeof (celula));
+	nova->chave = y;
+	nova->prox = t->prox;
+	t->prox = nova;
+}
+
+int desempilha_enc_C(celula *t)
+{
+	int x;
+	celula *p;
+	if (t->prox != NULL) {
+		p = t->prox;
+		x = p->chave;
+		t->prox = p->prox;
+		free(p);
+		return x;
+	}
+	else {
+		return 0;
+	}
+}
+
+int bemFormada(char *t)
+{
+	celula *lista;
+	char c;
+
+	lista = (celula *) malloc(sizeof(celula));
+	lista->prox = NULL;
+	
+	while(*t != '\0')
+	{
+		if(*t == '(')
+			empilha_enc_C(*t, lista);
+		
+		if(*t == ')') {
+			c = desempilha_enc_C(lista);
+			if(c == 0) 
+				return 0;
+			if(*t == '(' && c == ')') 
+				return 0;
+			}
+		t++;
+	}
+
+	if(lista->prox == NULL)
+		return 1;
+	else
+		return 0;
+}
+
+int main() {
+	
+	char cadeia[MAX];
+
+	while(scanf("%s", cadeia) != EOF) {
+		
+		if(bemFormada(cadeia) == 0)
+			printf("incorrect\n");
+		else
+			printf("correct\n");
+	}
+
+	return 0;
+}
